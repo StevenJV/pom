289390cce983dbf7e6e9f2f7bde6ee5448009e3e
Steven Vore <steven@vorefamily.net>
2023-09-05 11:48:00 -0400

    support

--- Usings.cs
+++ Usings.cs
@@ -1,5 +1,7 @@
 global using Xunit;
+global using Xunit.Abstractions;
 global using OpenQA.Selenium;
 global using OpenQA.Selenium.Firefox;
 global using SeleniumExtras.PageObjects;
+
 global using OpenQA.Selenium.Support.UI;
