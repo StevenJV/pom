99607729d4820b16f77ad6290b0fcb4e0543259a
Steven Vore <steven@vorefamily.net>
2023-09-13 11:50:58 -0400

    minor cleanup

--- Usings.cs
+++ Usings.cs
@@ -3,5 +3,4 @@ global using Xunit.Abstractions;
 global using OpenQA.Selenium;
 global using OpenQA.Selenium.Firefox;
 global using SeleniumExtras.PageObjects;
-
 global using OpenQA.Selenium.Support.UI;
