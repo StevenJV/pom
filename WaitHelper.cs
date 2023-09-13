7dee97a479c664c658075797a67149a28c62ca77
Steven Vore <steven@vorefamily.net>
2023-09-13 11:50:50 -0400

    resolves warnings. Not sure I'm happy with this

--- WaitHelper.cs
+++ WaitHelper.cs
@@ -1,31 +1,15 @@
 public static class WaitHelper
 {
     // use: element = driver.WaitUntilVisible(By.XPath("//input[@value='Save']"));
-    public static IWebElement WaitUntilVisible(
-        this IWebDriver driver,
-        By itemSpecifier,
-        int secondsTimeout = 10)
+    public static IWebElement? WaitUntilVisible(this IWebDriver driver,
+                                                By itemSpecifier,
+                                                int secondsTimeout = 10)
     {
         var wait = new WebDriverWait(driver, new TimeSpan(0, 0, secondsTimeout));
         var element = wait.Until<IWebElement>(driver =>
         {
-            try
-            {
-                var elementToBeDisplayed = driver.FindElement(itemSpecifier);
-                if (!elementToBeDisplayed.Displayed)
-                {
-                    return null;
-                }
-                return elementToBeDisplayed;
-            }
-            catch (StaleElementReferenceException)
-            {
-                return null;
-            }
-            catch (NoSuchElementException)
-            {
-                return null;
-            }
+            IWebElement? elementToBeDisplayed = driver.FindElement(itemSpecifier);
+            return elementToBeDisplayed;
         });
         return element;
     }
