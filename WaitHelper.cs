51953f083dafdb299c24046764ccf3ed999dbfeb
Steven Vore <steven@vorefamily.net>
2023-09-21 11:32:31 -0400

    forgive null

--- WaitHelper.cs
+++ WaitHelper.cs
@@ -1,15 +1,29 @@
 public static class WaitHelper
 {
     // use: element = driver.WaitUntilVisible(By.XPath("//input[@value='Save']"));
-    public static IWebElement? WaitUntilVisible(this IWebDriver driver,
-                                                By itemSpecifier,
-                                                int secondsTimeout = 10)
+    public static IWebElement WaitUntilVisible(
+        this IWebDriver driver,
+        By itemSpecifier,
+        int secondsTimeout = 10)
     {
         var wait = new WebDriverWait(driver, new TimeSpan(0, 0, secondsTimeout));
         var element = wait.Until<IWebElement>(driver =>
         {
-            IWebElement? elementToBeDisplayed = driver.FindElement(itemSpecifier);
-            return elementToBeDisplayed;
+            try
+            {
+                var elementToBeDisplayed = driver.FindElement(itemSpecifier);
+                if (!elementToBeDisplayed.Displayed)
+                {
+                    return null!;
+                }
+                return elementToBeDisplayed;
+            }
+            catch (Exception ex)
+            {
+                //TODO: implement logging
+                //Console.WriteLine("{1} looking for {2}", ex.Message, itemSpecifier.ToString());
+                return null!;
+            }
         });
         return element;
     }
