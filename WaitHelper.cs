7584fecf948d93c93007a2322ebd23ce4daa2c90
Steven Vore <steven@vorefamily.net>
2023-10-09 11:32:19 -0400

    exposes the error directly

--- WaitHelper.cs
+++ WaitHelper.cs
@@ -9,21 +9,12 @@ public static class WaitHelper
         var wait = new WebDriverWait(driver, new TimeSpan(0, 0, secondsTimeout));
         var element = wait.Until<IWebElement>(driver =>
         {
-            try
-            {
                 var elementToBeDisplayed = driver.FindElement(itemSpecifier);
                 if (!elementToBeDisplayed.Displayed)
                 {
                     return null!;
                 }
                 return elementToBeDisplayed;
-            }
-            catch (Exception ex)
-            {
-                //TODO: implement logging
-                //Console.WriteLine("{1} looking for {2}", ex.Message, itemSpecifier.ToString());
-                return null!;
-            }
         });
         return element;
     }
